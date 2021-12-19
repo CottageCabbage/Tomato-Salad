7be47881038b4b11fbd0a9d6150763a6df4ca705
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2021-12-19 16:41:10 -0300

    better CSS, split time in seconds with js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-
-
-
 function CoolFunc() {
 	timer = document.getElementById('inputTime').value;
 	console.log(timer)
-	document.getElementById('TitleHeading').innerHTML = timer;
+	timerTime = timer.split(":")
+	document.getElementById('timerPlace').innerHTML = timerTime;
+
+	seconds = [(timerTime[0]*3600), (timerTime[1]*60), (timerTime[2]*1)]
+	console.log(seconds)
 }
