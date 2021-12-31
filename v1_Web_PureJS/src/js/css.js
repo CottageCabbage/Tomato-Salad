318b1b41a3d5d3fc050030d946a4b2f5ca75d755
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2021-12-30 22:42:43 -0300

    Rotate

--- v1_Web_PureJS/src/js/css.js
+++ v1_Web_PureJS/src/js/css.js
@@ -6,9 +6,13 @@ function toogleSettings() {
 	if ('openSettings' == a[1]) {
 		document.getElementById("settingsBlock").classList.add('closeSettings');
 		document.getElementById("settingsBlock").classList.remove('openSettings'); 		
+		document.querySelector("#asd").style.transform = "rotate(360deg)";
 	} 
 	else if ('closeSettings' == a[1] | undefined == a[1]) {
 		document.getElementById("settingsBlock").classList.add('openSettings');
-		document.getElementById("settingsBlock").classList.remove('closeSettings'); 		
+		document.getElementById("settingsBlock").classList.remove('closeSettings'); 
+		document.querySelector("#asd").style.transform = "rotate(180deg)";	
 	}
+
+
 }
