a6c10ddd3c16342d25acc5265e546f039cbd7005
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2022-02-27 07:43:17 -0300

    Created presetItem as component

---
+++ src/components/presetItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ presetValues.workLen }}
+    {{ presetValues.shortBreakLen }}
+    {{ presetValues.longBreakLen }}
+    {{ presetValues.loopsTim }}
+  </div>
+</template>
+<script>
+export default {
+  props: ['presetValues'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    lol () {
+      alert('hello')
+    }
+  }
+}
+</script>
--- src/views/Presets.vue
+++ src/views/Presets.vue
@@ -1,25 +1,23 @@
 <template>
   <h1>Presets</h1>
 
-  <div v-for="(preset, i) in presetsOBJ" :key="i">
+  <div v-for="(preset, i) in presetsOBJ" :key="i" >
     {{ preset[0] }} |
-    {{ preset[0].workLen }} |
-    {{ preset[0].shortBreakLen }} |
-    {{ preset[0].longBreakLen }} |
-    {{ preset[0].loopsTim }}
-    <!-- <span v-for="(value, k) in preset" :key="k">
-      {{ value }} |
-    </span> -->
+    <presetItem
+      :presetValues="preset[0]"
+    />
   </div>
 
 </template>
 <script>
 import '@/assets/styles/presets.css'
+import PresetItem from '@/components/presetItem.vue'
 
 export default {
   mounted () {
     this.getPresets()
   },
+  components: { PresetItem },
   data () {
     return {
       presetsOBJ: ''
